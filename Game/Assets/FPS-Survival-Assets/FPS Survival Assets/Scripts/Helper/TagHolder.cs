a368295fb8bd1d7216a93ba13428cf3a7b3d2e88
marckes04 <maresp95@gmail.com>
2022-09-27 12:01:37 -0500

    first commit

--- Game/Assets/FPS-Survival-Assets/FPS Survival Assets/Scripts/Helper/TagHolder.cs
+++ Game/Assets/FPS-Survival-Assets/FPS Survival Assets/Scripts/Helper/TagHolder.cs
@@ -6,5 +6,6 @@ public class Axis
 {
     public const string HORIZONTAL = "Horizontal";
     public const string VERTICAL = "Vertical";
-
+    public const string MOUSE_VERTICAL = "Mouse Y";
+    public const string MOUSE_HORIZONTAL = "Mouse X";
 }
